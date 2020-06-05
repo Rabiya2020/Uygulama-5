b5caf8388e05a45be62a8094eee751b8385bebcd
Rabiya2020 <65968900+Rabiya2020@users.noreply.github.com>
2020-06-05 18:34:32 +0300

    Add files via upload

---
+++ Uygulama5/src/uygulama5/Uygulama5.java
@@ -0,0 +1,21 @@
+
+package uygulama5;
+
+
+public class Uygulama5 {
+
+   
+    public static void main(String[] args) 
+    {
+        topla();
+        //Ana metodumuz olan main metodumuzun içerisinde işlemimiizin sonucunu ekrana yazdırıyor.
+    }
+    
+    static void topla()
+    //Geriye değer döndürmeyen ve parametresiz topla adında metod oluşturduk.
+    {
+        System.out.println("Sonuç = "+ (13+5));
+        //Bu metod 13 ile 5 i toplayarak ekrana yazdırıyor.
+    }
+    
+}
